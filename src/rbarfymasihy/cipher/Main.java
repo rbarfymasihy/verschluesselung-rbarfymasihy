77d55bf5ef0d4aa310f792600671e21d6a39f9e3
rbarfymasihy <rbarfymasihy@student.tgm.ac.at>
2024-01-07 19:31:06 +0100

    Initial commit

---
+++ src/rbarfymasihy/cipher/Main.java
@@ -0,0 +1,11 @@
+package rbarfymasihy.cipher;
+
+/** Dies ist die Main-Klasse des Projekts.
+ *
+ * @author  rbarfymasihy
+ * @version 07.01.2024
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
